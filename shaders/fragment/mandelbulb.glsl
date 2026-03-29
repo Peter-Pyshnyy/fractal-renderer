6732d7116bccc310c7709ca38d4312c13119ca2c
Peter-Pyshnyy <pyshnyy.peter@gmail.com>
2026-03-29 23:31:18 +0200

    standardize RM algorithms for fair comparisson

--- shaders/fragment/mandelbulb.glsl
+++ shaders/fragment/mandelbulb.glsl
@@ -7,8 +7,9 @@ layout(set = 0, binding = 0) uniform writeonly image2D output_image;
 
 #include "res://shaders/includes/shared_data.gdshaderinc"
 #include "res://shaders/includes/sdfs/sdf_mandelbulb.gdshaderinc"
-#include "res://shaders/includes/rayMarcher/ray_marcher_AR.gdshaderinc"
-//#include "res://shaders/includes/rayMarcher/ray_marcher_enhanced.gdshaderinc"
+//#include "res://shaders/includes/rayMarcher/ray_marcher.gdshaderinc" 
+#include "res://shaders/includes/rayMarcher/ray_marcher_enhanced.gdshaderinc"
+//#include "res://shaders/includes/rayMarcher/ray_marcher_AR.gdshaderinc"
 
 void main() {
     // Дізнаємося базову координату для нашого "товстого" пікселя
@@ -21,10 +22,10 @@ void main() {
     vec2 offset = vec2(float(params.resolution_scale) * 0.5);
     vec2 uv = (vec2(base_coord) + offset) / vec2(image_size);
     uv.y = 1.0 - uv.y;
-
+     
     vec3 rayDir = getRayDirection(cam.resolution, uv);
-    vec3 color = raymarch_AR(rayDir);
-
+    vec3 color = raymarch_enhanced(rayDir);
+     
     // Записуємо один і той самий колір у всі пікселі квадрата (наприклад, 2x2)
     for (int y = 0; y < params.resolution_scale; y++) {
         for (int x = 0; x < params.resolution_scale; x++) {
@@ -32,6 +33,6 @@ void main() {
             if (write_coord.x < image_size.x && write_coord.y < image_size.y) {
                 imageStore(output_image, write_coord, vec4(color, 1.0));
             }
-        }
-    }
-}
+        } 
+    } 
+} 
