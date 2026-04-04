28cf382d85b7a62d11a9f73f5db99c994e96ccef
Peter-Pyshnyy <62466617+Peter-Pyshnyy@users.noreply.github.com>
2026-04-04 15:56:55 +0200

    Add FractalMaterial class

---
+++ scripts/fractal_material.gd
@@ -0,0 +1,5 @@
+class_name FractalMaterial
+extends Resource
+
+@export var color0: Color = Color.BISQUE
+@export var color1: Color = Color.BROWN
--- scripts/global.gd
+++ scripts/global.gd
@@ -1,3 +1,4 @@
 class_name Global
 
 static var g_fractal: FractalData
+static var g_active_material: FractalMaterial
